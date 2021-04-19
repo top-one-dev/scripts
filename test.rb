7890eb0d7a710aa7b71f0af9721b666c72fbfdf3
Top One Dev <toponedev@gmail.com>
2021-04-18 21:49:12 -0400

    Ruby - Enumerable - collect

--- test.rb
+++ test.rb
@@ -1,5 +1,16 @@
-def skip_animals(animals, skip)
-    result = []
-    animals.each_with_index{ |e, i| result.push "#{i}:#{e}" if i >= skip }
-    result
+def rot13(secret_messages)
+  # your code here
+    a = ('a'..'z').to_a
+    z = ('A'..'Z').to_a
+    secret_messages.chars.map{ |s| 
+        unless s.match?(/[[:alpha:]]/)
+            s
+        else
+        	if s == s.downcase 
+	        	a[ ( a.index(s) + 13 ) % a.count ]
+	        else
+	            z[ ( z.index(s) + 13 ) % z.count ]
+	        end
+	    end
+    }.join("")
 end
