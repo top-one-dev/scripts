7d7bb4cf8a372f6b2df1d80e7d84597f0cf809be
Top One Dev <toponedev@gmail.com>
2021-04-18 22:20:51 -0400

    Ruby - Enumerable - reduce

--- test.rb
+++ test.rb
@@ -1,18 +1,5 @@
-def rot13(secret_messages)
-  # your code here
-    a = ('a'..'z').to_a
-    z = ('A'..'Z').to_a
-    secret_messages.map{ |message|
-        message.chars.map{ |s| 
-            unless s.match?(/[a-zA-Z]/)
-                s
-            else
-                if s == s.downcase 
-                    a[ ( a.index(s.to_s) + 13 ) % a.count ]
-                else
-                    z[ ( z.index(s.to_s) + 13 ) % z.count ]
-                end
-            end
-        }.join("")
-    }
+def sum_terms(n)
+  1.upto(n).reduce(0) do |m, x|
+    m + x*x + 1
+  end
 end
