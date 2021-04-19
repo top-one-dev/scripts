049fb533974de355935b08d29a0b0c5d9ab81bf6
Top One Dev <toponedev@gmail.com>
2021-04-18 22:02:42 -0400

    Ruby - Enumerable - collect 1

--- test.rb
+++ test.rb
@@ -2,15 +2,17 @@ def rot13(secret_messages)
   # your code here
     a = ('a'..'z').to_a
     z = ('A'..'Z').to_a
-    secret_messages.chars.map{ |s| 
-        unless s.match?(/[[:alpha:]]/)
-            s
-        else
-        	if s == s.downcase 
-	        	a[ ( a.index(s) + 13 ) % a.count ]
-	        else
-	            z[ ( z.index(s) + 13 ) % z.count ]
-	        end
-	    end
-    }.join("")
+    secret_messages.map{ |message|
+        message.chars.map{ |s| 
+            unless s.match?(/[a-zA-Z]/)
+                s
+            else
+                if s == s.downcase 
+                    a[ ( a.index(s.to_s) + 13 ) % a.count ]
+                else
+                    z[ ( z.index(s.to_s) + 13 ) % z.count ]
+                end
+            end
+        }.join("")
+    }
 end
